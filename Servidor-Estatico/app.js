ef2ecb0f1cd3166ff49dc7e15f8cbf05d90c61a4
GloriaRamosM <gloriaramosmoran@gmail.com>
2023-10-29 16:04:25 -0300

    Servidor estatico con Nodejs. Tanto en texto simple, como renderizando un archivo HTML

--- Servidor-Estatico/app.js
+++ Servidor-Estatico/app.js
@@ -1,13 +1,43 @@
+// COMENTARODIGO CONTOL + K + C .. DESCOMENTAR CONTOL + K + U
+
+// PRIMER EJEMPLO : SERVIDOR ESTATICO CON UNA PAGINA Y TEXTO SIMPLE
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, {
+//     "Content-Type": "text/plain",
+//   });
+
+//   res.end(" Hola mundo, te saludo desde la terminal");
+// });
+
+// server.listen(4500, () =>
+//   console.log("Servidor corriendo en puerto 4500, http://localhost:4500")
+// );
+
+// SEGUNDO EJEMPLO: SERVIDOR ESTATICO CON UN TEXTO COMO HTML. CAMBIA EL CONTENT Y EL END
+
+//
+
+// TERCER EJEMPLO : SERVDOR ESTATICO PERO LEVANTANDO EL ARCHIVO DE INDEX HTML
+
 const http = require("http");
+const fs = require("fs");
+const { __dirname } = require("path");
 
 const server = http.createServer((req, res) => {
+  const file = fs.readFileSync(__dirname + "/index.html");
+  //console.log(__dirname) por si necesito ver que esta haciendo
+
   res.writeHead(200, {
-    "Content-Type": "text/plain",
+    "content-type": "text/html; charset=utf-8",
   });
 
-  res.end(" Hola mundo, te saludo desde la terminal");
+  res.end(file);
 });
 
 server.listen(4500, () =>
-  console.log("Servidor corriendo en puerto 4500, http://localhost:4500")
+  console.log(
+    " Este servido esta corriendo en puerto 4500, http://localhost:4500"
+  )
 );
